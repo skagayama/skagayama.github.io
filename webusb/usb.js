7d3364caa633121d179dbf5cfcd5d6e4214c5533
skagayama <s-kagayama@artec-kk.co.jp>
2020-01-17 12:33:31 +0900

    webusb test

---
+++ webusb/usb.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const auth = () => {
+    const vendorId = 8352; // ここにVendor ID
+    const productId = 17001; // ここにProduct ID
+    // const vendorId = '20a0'; // ここにVendor ID
+    // const productId = '4269'; // ここにProduct ID
+    // const serialNumber = 'XXXXXXXX'; // ここにSerial Number
+    navigator.usb.requestDevice(
+        {
+        'filters': [
+            { 'vendorId': vendorId, 'productId': productId }
+        ]
+        }
+    ).then(device => {
+        console.log(device);
+        // if (device.serialNumber === serialNumber) {
+        //     console.log('success!');
+        //     login();
+        //     return device.open;
+        // } else {
+        //     console.log('false!');
+        //     alert('認証に失敗しました');
+        // }
+    }).catch(error => {
+        console.log(error);
+    });
+}
+document.getElementById('connect').addEventListener("click", auth, false);
